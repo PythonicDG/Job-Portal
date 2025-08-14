fd7dfdb83b2ce6970fbf10a3952bdbdb244e05b1
PythonicDG <dipakgaikwadmg@gmail.com>
2025-08-14 11:11:38 +0530

    [fix]  Response not defined

--- backend/job_portal/frontend/views.py
+++ backend/job_portal/frontend/views.py
@@ -8,6 +8,7 @@ from .models import *
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .utils import send_contact_email
+from rest_framework.response import Response
 
 
 @api_view(['GET'])
