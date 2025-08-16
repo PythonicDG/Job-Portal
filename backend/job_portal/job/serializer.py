ba2cba2d09f3664265341302ac0158f1e10405c6
PythonicDG <dipakgaikwadmg@gmail.com>
2025-08-16 16:05:26 +0530

    [modify] added logo in saved_jobs api

--- backend/job_portal/job/serializer.py
+++ backend/job_portal/job/serializer.py
@@ -24,7 +24,7 @@ class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = [
-            'job_id', 'title', 'is_remote', 'employment_type',
+            'job_id', 'title', 'is_remote', 'employment_type', 'employer_logo',
             'posted_at', 'google_link', 'min_salary', 'max_salary', 'salary_period',
             'employer', 'location', 'apply_option'
         ]
