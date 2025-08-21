8035bc208307e62ca7ccfd0d7383d8ff8c43824d
PythonicDG <dipakgaikwadmg@gmail.com>
2025-08-21 15:34:28 +0530

    [fix]

--- backend/job_portal/job/signals.py
+++ backend/job_portal/job/signals.py
@@ -1,16 +1,16 @@
 
-    from django.dispatch import Signal, receiver
-    from .models import Notification, SiteUser
+from django.dispatch import Signal, receiver
+from .models import Notification, SiteUser
 
 
-    jobs_synced = Signal()
+jobs_synced = Signal()
 
-    @receiver(jobs_synced)
-    def create_jobs_notification(sender, total_jobs, **kwargs):
-        if total_jobs > 0:
-            for user in SiteUser.objects.all():
-                Notification.objects.create(
-                    user=user,
-                    title="Jobs Synced",
-                    message=f"{total_jobs} new jobs were added successfully."
-                )
+@receiver(jobs_synced)
+def create_jobs_notification(sender, total_jobs, **kwargs):
+    if total_jobs > 0:
+        for user in SiteUser.objects.all():
+            Notification.objects.create(
+                user=user,
+                title="Jobs Synced",
+                message=f"{total_jobs} new jobs were added successfully."
+            )
